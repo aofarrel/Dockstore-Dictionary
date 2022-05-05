9de64ef94f593ce64b5c92d56567d2c506055534
aofarrel <aofarrel@ucsc.edu>
2022-05-05 16:21:15 -0700

    is --> ==

--- Gloss.py
+++ Gloss.py
@@ -15,7 +15,7 @@ class GlossEntry:
 		# acronym_full (string) - if acronym, what is the full name. if blank, assumed to not be an acronym
 		# further_reading (string) - URL to a webpage, usually an "official" one associated with the term
 		# institute (string) - optional, which institution is the phrase associated with, if any? For instance, Terra is associated with the Broad Institute. GCP is associated with Google. Does not necessarily imply a trademark or copyright.
-		# internal (bool) - Is this only used internally? For acronyms this may imply the acronym is internal but the phrase it refers to is not (ex: gha - github app)
+		# internal (bool) - Is this only used internally? For acronyms this may imply the acronym is internal but the phrase it refers to is not(ex: gha - github app)
 		# pronunciation (string) - optional pronunciation (ex: wdl - "widdle")
 		# updated (date) - when the entry was last updated
 
@@ -31,30 +31,30 @@ class GlossEntry:
 			f.write("\n")
 
 			# print pronunciation
-			if self.pronunciation is not "":
+			if self.pronunciation !== "":
 				f.write(f"[pronounced {self.pronunciation}]\n")
 
 			# print full form of acronym or see also
 			# for an acronym, seealso will replace the full form of the acronym
 			# this section could probably be written cleaner
-			if self.acronym_full is not "" and self.seealso is not "":
+			if self.acronym_full !== "" and self.seealso !== "":
 				f.write(f"see {self.seealso}\n")
-			elif self.acronym_full is not "" and self.seealso is "":
+			elif self.acronym_full !== "" and self.seealso == "":
 				f.write(f"abbreviation for {self.acronym_full}\n")
-			elif self.acronym_full is "" and self.seealso is not "":
+			elif self.acronym_full == "" and self.seealso !== "":
 				f.write(f"see {self.seealso}\n")
 			else:
 				pass
 
 
-			if self.definition is not "":
+			if self.definition !== "":
 				f.write(f"	{self.definition}\n")
 
-			if self.institute is not "":
+			if self.institute !== "":
 				f.write(f"This term as we define it here is associated with {self.institute} and may have different definitions in other contexts.\n")
 			
 
-			if self.furtherreading is not "":
+			if self.furtherreading !== "":
 				f.write(f"Further reading: {self.furtherreading}\n")
 
 			f.write(self.updated.strftime("updated %Y-%m-%d\n"))
