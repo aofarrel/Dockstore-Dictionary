4a90d542b529499d140e14d82ee4312963bdb5d6
aofarrel <aofarrel@ucsc.edu>
2022-05-06 15:32:34 -0700

    implement alphabetical sorting with lambda

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from entries import * 
 import gc
 
+list_entries = []
 for glossary_object in gc.get_objects():
     if isinstance(glossary_object, GlossEntry):
-        glossary_object.generate_plaintext()
+        list_entries.append(glossary_object)
+
+list_entries.sort(key=lambda x: x.name.upper())
+
+for entry in list_entries:
+    entry.generate_plaintext()
