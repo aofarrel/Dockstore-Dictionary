ab1747b7f6d3a26251ec7eee178cd077a0b772df
aofarrel <aofarrel@ucsc.edu>
2022-05-10 12:10:13 -0700

    TOC now has three columns

--- main.py
+++ main.py
@@ -12,17 +12,23 @@ for glossary_object in gc.get_objects():
 
 list_entries.sort(key=lambda x: x.name.upper())
 
-with open(outfile, "a") as f:
+with open(contents, "a") as f:
+    for entry in list_entries:
+        # not using text_rst_bookmark() to make this more human-readable
+        # and so RST TOC doesn't have to process `.. _` and `:` bits
+        # if bookmark format ever changes this may need be adjusted
+        f.write(f"{entry.return_name()}\n") 
+
+with open(outfile, "a") as g:
     # generate RST page title
-    f.write("Dockstore Dictionary\n")
-    f.write("====================\n")
-    # tell RST to make a local TOC
-    f.write(".. contents:: Table of Contents\n\t:local:\n\n")
-    #f.write(".. hlist:: \n\t:columns: 3\n\n\t* A list of\n\t* short items\n\t* to display horizontally\n\n")
+    g.write("Dockstore Dictionary\n")
+    g.write("====================\n")
+    # tell RST to make a local TOC from toc.txt
+    g.write(".. hlist:: \n\t:columns: 3\n\n")
+    with open(contents, "r") as h:
+        for line in h.readlines():
+            g.write(f"\t* :ref:`dict {line[:-1]}`\n")
+    g.write("\n") #needed to avoid RST getting mad
     # generate main body text
     for entry in list_entries:
-        f.write(entry.generate_RST())
-
-with open(contents, "a") as g:
-    for entry in list_entries:
-        g.write(f"{entry.return_name()}\n") 
+        g.write(entry.generate_RST())
