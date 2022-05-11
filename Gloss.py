e32c2413c0878afe05e728e16da9ee4061d486c8
aofarrel <aofarrel@ucsc.edu>
2022-05-11 16:24:45 -0700

    cleanup functions

--- Gloss.py
+++ Gloss.py
@@ -1,28 +1,23 @@
 import datetime
 import re
 
-# Questions about best practices
-#
-# I want the user to be able to initalize objects without specifying all possible variables. Is it
-# more correct to initalize the variables as None and later check if they are None, or to initalize
-# them as empty strings (all are str type) and later check if they == ""?
-
 class GlossEntry:
 	'''Object for an individual glossary entry'''
-	def __init__(self, name, acronym_full="", definition="", furtherreading="", institute="", pronunciation="", seealso="", updated=datetime.date.today()):
+	def __init__(self, name, acronym_full="", definition="", furtherreading="", institute="",
+		pronunciation="", seealso="", updated=datetime.date.today()):
 		'''
-		name - entry's name
+		name - entry's name; spaces are supported, do not use [brackets]
 		acronym_full - if acronym, what is the full name. if blank, assumed to not be an acronym.
 		further_reading - URL to a webpage, usually an "official" one associated with the term
 		institute - which institution is the phrase associated with?
-		pronunciation - optional pronunciation (ex: wdl - "widdle")
+		pronunciation - pronunciation (ex: wdl - "widdle")
 		seealso - related but not equivalent entries, such as CLI being related to Dockstore CLI. 
 		updated - when the entry was last updated
 
 		When outputting to RST, acronym_full, seealso, & definition will replace text in [brackets] 
 		with a working internal hyperlink to another entry. For example, if self.definition="I use 
 		[Seven Bridges]", then the RST out will be "I use :ref:`dict Seven Bridges`" (although the
-		link will not be clickable if there is no
+		link will not be clickable if there is no entry for Seven Bridges)
 		'''
 		self.name: str = name
 		self.acronym_full: str = acronym_full
@@ -34,7 +29,7 @@ class GlossEntry:
 		self.updated: datetime = updated
 
 	def return_name(self, nospaces=False):
-		'''Can be used to generate an RST bookmark, or, if entry is in a list, a quick way to make rudimentary TOC'''
+		'''Returns name of the entry'''
 		if nospaces==False:
 			return self.name
 		else:
@@ -100,13 +95,15 @@ class GlossEntry:
 		return "".join(entry_title)
 
 	def text_pronunciation(self, format="txt"):
+		'''Return pronunciation'''
 		if format == "txt":
 			return f"[pronounced {self.pronunciation}]\n"
 		elif format == "rst":
 			return f"pronounced {self.pronunciation}  \n\n"
 
 	def text_acronym(self, format="txt"):
-		'''Return acronym's full form, in italics if RST. We need an extra newline in RST to stop the acronym from being considered the header title of the definition.'''
+		'''Return acronym's full form, in italics if RST. We need an extra newline in RST to prevent
+		the acronym from being considered a header relative to the definition.'''
 		if format == "txt":
 			return f"abbreviation for {self.acronym_full}\n"
 		elif format == "rst":
@@ -114,9 +111,9 @@ class GlossEntry:
 			return f"*abbreviation for* {self._process_internal_link_brackets_(words)}  \n\n"
 
 	def text_definition(self, format="txt"):
-		'''If RST, we need to make the [references to other entries] into internal links.
+		'''Return the definition of the entry.
+		If RST, calls a function that turns bracketed [text] into internal links.
 		Limitations: RST does not support letters coming after a link without a puncuation mark.'''
-
 		if format == "txt":
 			return f"	{self.definition}\n"
 		elif format == "rst":
@@ -125,26 +122,30 @@ class GlossEntry:
 			return f"	{processed_with_links}  \n\n"
 
 	def text_institute(self, format="txt"):
-		'''In RST form this becomes a note block.'''
+		'''Return a caveat about how this term may mean something else outside the context of
+		self.institute. In RST form this becomes a note block.'''
 		if format == "txt":
 			return f"This term as we define it here is associated with {self.institute} and may have different definitions in other contexts.\n"
 		elif format == "rst":
 			return f".. note:: This term as we define it here is associated with {self.institute} and may have different definitions in other contexts.  \n"
 		
 	def text_seealso(self, format="txt"):
-		'''There is a supposedly simplier way to do this with sphinx.ext.autosectionlabel, via:
+		'''Returns the entry's seealso information, which links to another entry.
+		There is a supposedly simplier way to do this with sphinx.ext.autosectionlabel, via:
 			see also :ref:`{self.seealso}`
-		...but that would require we reformat piles of documentation (175 errors!)'''
+		...but using extension often requires people reformat tons of links. For example, in
+		Dockstore's documentation, loading sphinx.ext.autosectionlabel raises 175 new errors.'''
 		if format == "txt":
 			return f"see also {self.seealso}\n"
 		elif format == "rst":
 			if self.institute == "":
 				return f"see also :ref:`dict {self.seealso}`  \n"
-			else:  # not strictly necessary to render correctly, but without this warning will be thrown
+			else:  # not strictly necessary to render, but without this warning will be thrown
 				return f"\nsee also :ref:`dict {self.seealso}`  \n"
 
 	def text_furtherreading(self, format="txt"):
-		'''If there is a see also, we need an extra newline. If not, avoid the extra newline.'''
+		'''Returns the entry's further reading section, which is a single URL.
+		In RST, if there is a see also, we need an extra newline in further reading.'''
 		if format == "txt":
 			return f"Further reading: {self.furtherreading}\n"
 		elif format == "rst":
@@ -154,8 +155,9 @@ class GlossEntry:
 				return f"\nFurther reading: `<{self.furtherreading}>`_  \n"
 
 	def text_updated(self, format="txt"):
-		'''Ideally, should print when entry was last updated visibly if plaintext, as a comment if RST.
-		But the current implementation of entries.py gives all entries the same timestamp.'''
+		'''Return when entry was last updated (visibly if txt, as a comment if RST)
+		Caveat: This isn't very helpful if you keep all of your entries in the same file and
+		parse all of them at the same time.'''
 		if format == "txt":
 			return self.updated.strftime("updated %Y-%m-%d\n")
 		elif format == "rst":
@@ -208,13 +210,19 @@ class GreatGloss:
 		self.outfile: str = outfile
 		self.outtoc: str = outtoc
 		self.updated: datetime = updated
-		self.glosslist: list[GlossEntry] = []
-		self._gentries_: generator = self._generate_entries_()
+		self.glosslist: list[GlossEntry] = []  # updated by add_entry
 
 	def add_entry(self, entry:GlossEntry):
+		'''Add a new entry to the glossary'''
 		self.glosslist.append(entry)
 
-	def _generate_entries_(self, asRSTlinks=False):
+	def add_entries(self, entries:list):
+		'''Add a list of entries to the glossary'''
+		for entry in entries:
+			self.add_entry(entry)
+
+	def _generate_entry_names_(self, asRSTlinks=False):
+		'''Yield the name of every entry. If asRSTlinks==True, make them clickable internal links'''
 		for entry in self.glosslist:
 			if asRSTlinks:
 				# remember: _process_internal_link_brackets_() expects input as a list, not str!
@@ -223,13 +231,14 @@ class GreatGloss:
 				yield f"{entry.return_name()}\n"
 
 	def sort_entries(self, ignorecase=True):
+		'''Alphabetically sort all entries by their name field'''
 		if ignorecase:
 			self.glosslist.sort(key=lambda x: x.name.upper())
 		else:
 			self.glosslist.sort(key=lambda x: x.name)
 
-	def make_toc(self, format="rst", columns=3):
-		'''generates a TOC
+	def make_toc(self, format, columns=3):
+		'''Generates a TOC as a list of strings
 		if columns>=1 && format=="rst": number of hlist columns to use
 		if columns<=0 && format=="rst": use Sphinx built in local TOC instead of hlist'''
 		TOC = []
@@ -238,41 +247,41 @@ class GreatGloss:
 				TOC.append(".. contents:: Table of Contents \n\t:local:\n\n")
 			else:
 				TOC.append(f".. hlist:: \n\t:columns: {columns}\n\n")
-			for entry in self._generate_entries_(asRSTlinks=True):
+			for entry in self._generate_entry_names_(asRSTlinks=True):
 				TOC.append(f"\t* {entry}\n")
 		else:
-			for entry in self._generate_entries_(asRSTlinks=False):
+			for entry in self._generate_entry_names_(asRSTlinks=False):
 				TOC.append(entry)
-		print(TOC)
 		return TOC
 
 
 	def write_glossary(self, outfile="", format="rst", skipTOC=False):
-		'''write a glossary to a file, in plaintext or RST formatting'''
+		'''Write a glossary to a file, in plaintext or RST formatting.
+		will try to fall back on self.outfile if outfile not declared when calling this function'''
 		if outfile=="" and self.outfile=="":
 			raise RuntimeError("No output file for glossary specified")
 		with open(outfile if outfile!="" else self.outfile, "a") as f:
 			f.write("".join(self.text_glossary_title()))
 			if not skipTOC:
-				f.write("".join(self.make_toc()))
-				#
-				#
-				# toc stuff
-				#
-				#
+				f.write("".join(self.make_toc(format=format)))
 				f.write("\n")  # needed to keep RST from getting mad
 			for entry in self.glosslist:
-				entry.generate_RST()
+				f.write(entry.generate_RST())
 
-	def write_toc(self, outtoc="", format="txt"):
-		'''write a TOC to a file'''
+	def write_toc(self, outtoc="", format="txt", columns=0):
+		'''Write a table of contents to outtoc.
+		Will try to fall back on self.outtoc if outtoc not declared when calling this function
+		if columns>=1 && format=="rst": number of hlist columns to use
+		if columns<=0 && format=="rst": use Sphinx built in local TOC instead of hlist'''
 		if outtoc=="" and self.outtoc=="":
 			raise RuntimeError("No output file for TOC specified")
 		with open(outtoc if outtoc!="" else self.outtoc, "a") as f:
-			f.write("".join(self.make_toc(format=format)))
+			f.write("".join(self.make_toc(format=format, columns=columns)))
 
 	def text_glossary_title(self):
+		'''Generate the overall glossary's title'''
 		return self._underline_text_(self.title, underlinechar="=")
 
 	def _underline_text_(self, text:str, underlinechar="-"):
+		'''Underline text in a RST-supported way'''
 		return [f"{text}\n", underlinechar * len(text)+"\n"]
--- main.py
+++ main.py
@@ -14,26 +14,12 @@ except FileNotFoundError:
 
 
 # this feels dodgy, but it hasn't failed yet
+# todo: read entries.py instead of importing it (maybe)
 dockstore_dictionary = GreatGloss("Dockstore Dictionary")
 for glossary_object in gc.get_objects():
     if isinstance(glossary_object, GlossEntry):
         dockstore_dictionary.add_entry(glossary_object)
 
 dockstore_dictionary.sort_entries()
-dockstore_dictionary.write_toc(contents, format="rst")
+dockstore_dictionary.write_toc(contents)
 dockstore_dictionary.write_glossary(outfile)
-
-
-with open(outfile, "a") as g:
-    # generate RST page title
-    #g.write("Dockstore Dictionary\n")
-   # g.write("====================\n")
-    # tell RST to make a local TOC from toc.txt
-   # g.write(".. hlist:: \n\t:columns: 3\n\n")
-    #with open(contents, "r") as h:
-    #    for line in h.readlines():
-    #        g.write(f"\t* :ref:`dict {line[:-1]}`\n")
-    g.write("\n") #needed to avoid RST getting mad
-    # generate main body text
-    for entry in list_entries:
-        g.write(entry.generate_RST())
